2fb75b121073ac123a1b9b7579223b27ecce1bb3
Gheribi Abd-el-rahmen <117765552+TawahinBeirut@users.noreply.github.com>
2023-10-07 12:30:50 +0200

    Ajout de la nouvelle navbar pour mon cher et tendre coéquipier

--- Composants/navbar.php
+++ Composants/navbar.php
@@ -1,10 +1,10 @@
 <?php
     function display_Navbar(){
         echo '<header class="flex flex-row items-center justify-center p-4 w-full bg-white border-gray-200 shadow-gray-100">
-        <a href="index.php" class="mr-40"><img class="h-8" alt="logo" src="../images/logo-gradient.png"></img></a>
-        <form class="mr-auto my-0">
+        <a href="../Pages/Feed.php" class="mr-40"><img class="h-8" alt="logo" src="../images/logo-gradient.png"></img></a>
+        <form name="form" class="mr-auto my-0" action="Feed.php" method="get">
             <div class="flex flex-row w-50 bg-gray-100 p-2 px-4 rounded-xl">
-                <input type="search" id="default-search" class="w-full bg-transparent outline-none"
+                <input name="Search" type="search" id="default-search" class="w-full bg-transparent outline-none"
                     placeholder="Rechercher..." required>
                 <button type="submit"><svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
