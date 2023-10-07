9fcbbbe5d2fe2160a033cf25f38848db24d976d9
Gheribi Abd-el-rahmen <117765552+TawahinBeirut@users.noreply.github.com>
2023-10-07 23:29:32 +0200

    Page Article finie

--- Composants/Article_Comment.php
+++ Composants/Article_Comment.php
@@ -1,11 +1,15 @@
 <?php
     function display_Comment($comment){
-        // A finir ---> Créer le form pour delete le comm
+        // A finir ---> Créer le form pour delete le comm et mettre 
         echo '
         <div class="shadow-inner rounded-xl m-3 w-11/12 h-max bg-white">
-            <div class="m-2 font-semibold flex justify-between">' .$comment["Pseudo"] . '<button class=" hover:bg-red-400 rounded-xl p-1"><span class="material-symbols-outlined">
-            delete
-            </span></div>
+            <div class="m-2 font-semibold flex justify-between">' .$comment["Pseudo"] .
+            '<form name="form" action="../Controller.php" method="post">
+                <input name="Request" value="DeleteComment" hidden/>
+                <input name="Id" value ="' . $comment['id'] . '"hidden/>
+                <input name="IdArticle" value ="' . $comment['IdArticle'] . '"hidden/>
+                <button type="submit" class=" hover:bg-red-400 rounded-xl p-1"><span class="material-symbols-outlined">delete</span></div>
+            </form>
             <div class="m-2"><p class="font-normal"> ' . $comment["Description"] . '</p></div>
         </div>';
     }
--- Controller.php
+++ Controller.php
@@ -11,22 +11,35 @@
                 break;
             case 'PostComment' :
                 if (isset($_POST['Desc'])){
-                    $data = array("IdArticle" => $_POST['IdArticle'],"Description" => $_POST["Desc"],"Pseudo" => "Ihoudi45","IdUser" => 2);
+                    $data = array("IdArticle" => $_POST['IdArticle'],"Description" => $_POST["Desc"],"Pseudo" => "Ihoudi45","IdUser" => 2); // Utiliser les variables session pour le pseudo et l'IDUser
                     $comments = createGetRequest("http://localhost/Blog/API/index.php/Comments");
-                    $i = 1;
-                    $id = 0;
+                    
+                    // Code pour incrémenter les comments
+                    $usedId = array();
                     foreach($comments["Data"] as $comment){
-                        if ($comment["id"] != $i){
-                            $id = $i;
-                        }
-                        $i++;
+                        array_push($usedId,$comment['id']);
                     }
-                    if ($id==0) $id = $i +1;
+                    $stop = false;
+                    $id = 0;
+                    for($i=1;$i<=count($usedId);$i++){
+                        if(!in_array($i,$usedId) and !$stop){
+                            $id = $i;
+                            $stop = true;
+                        }
+                    }
                     $result = createPostRequest("http://localhost/Blog/API/index.php/Comments/" . $id,$data);
+
                     header('Location: Pages/Article.php?id=%20' . $_POST['IdArticle']);
+                    
                 }
+                else echo 'error veuillez retournez à la page précedente';
                 break;
             case 'DeleteComment':
+                if (isset($_POST['Id']) and isset($_POST['IdArticle'])){
+                    createDeleteRequest("http://localhost/Blog/API/index.php/Comments/" . $_POST['Id']);
+                    header('Location: Pages/Article.php?id=%20' . $_POST['IdArticle']);
+                }
+                else echo 'error veuillez retournez à la page précedente';
                 break;
             default :
             header('Location: feed.php');
